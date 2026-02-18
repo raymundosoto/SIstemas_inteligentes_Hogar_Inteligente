1be790093328de6929cb0c4789fd50a7f73c0f1e
Raymundo Soto <72757419+raymundosoto@users.noreply.github.com>
2026-02-18 11:17:16 -0600

    Actualizacion para considerar actuadores

--- app.py
+++ app.py
@@ -3,13 +3,11 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-# Diccionario para guardar agentes de forma independiente
-# Estructura: {"Nombre": {"hora": "...", "led1": 0, "led2": 0, "led3": 0}}
+# Almacenamos datos de agentes, actuadores y clima
 agentes = {}
 
 @app.route('/')
 def index():
-    # Pasamos el diccionario 'agentes' al HTML
     return render_template('index.html', agentes=agentes)
 
 @app.route('/update', methods=['POST'])
@@ -17,27 +15,16 @@ def update_status():
     datos = request.json
     id_esp = datos.get('id_dispositivo', 'Desconocido')
     
-    # Aseguramos que los valores sean enteros (0 o 1) para que el HTML los entienda
     agentes[id_esp] = {
         "hora": datetime.now().strftime("%H:%M:%S"),
-        "led1": int(datos.get('led1', 0)),
-        "led2": int(datos.get('led2', 0)),
-        "led3": int(datos.get('led3', 0))
+        "temp": datos.get('temp', 0),
+        "hum": datos.get('hum', 0),
+        "viento": datos.get('viento', 0),
+        "act1": int(datos.get('act1', 0)), # Ventilador
+        "act2": int(datos.get('act2', 0)), # Humidificador
+        "act3": int(datos.get('act3', 0))  # Iluminación
     }
     return jsonify({"status": "recibido"}), 200
-
-# Esta ruta es para que la App o el CMD cambien el estado
-@app.route('/orden', methods=['POST'])
-def recibir_orden():
-    datos = request.json
-    id_esp = datos.get('id_dispositivo')
-    led = datos.get('led')      # Ejemplo: "led1"
-    estado = datos.get('estado') # 1 o 0
-    
-    if id_esp in agentes:
-        agentes[id_esp][led] = int(estado)
-        return jsonify({"status": "ok"}), 200
-    return jsonify({"error": "Agente no encontrado"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
